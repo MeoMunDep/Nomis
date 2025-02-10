1d4cb1ea98f4f9df33e57a6daedbb77a98acea48
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-10 23:59:08 +0700

    fix all

--- Nomis/setup.bat
+++ Nomis/setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title Nomis Bot
+title Nomis Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    Nomis BOT SETUP AND RUN SCRIPT
+echo    Nomis BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -95,7 +95,7 @@ if exist "..\node_modules" (
 ) else (
     echo Using node_modules from current directory
 )
-cd nomis && node bot
+node MeoMunDep
 pause
 goto MENU
 
